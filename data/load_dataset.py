6458f6ca11f3084635e822b4538e6af82662e3fd
HeFasheng <2973051203@qq.com>
2025-05-03 21:03:16 +0800

    2025-5-3:修改相关代码适配k8s环境路径

--- data/load_dataset.py
+++ data/load_dataset.py
@@ -15,9 +15,6 @@ def load_mnist(batch_size=64):
     return DataLoader(test_set, batch_size=batch_size, shuffle=False)
 
 
-from torchvision import datasets, transforms
-from torch.utils.data import DataLoader
-
 
 def load_cifar(batch_size=64):
     # CIFAR-10专用归一化参数
@@ -28,7 +25,10 @@ def load_cifar(batch_size=64):
     ])
 
     test_set = datasets.CIFAR10(
-        root="./data/dataset",
+        # 本地运行使用下面的
+        # root="./data/cifar10",
+        # k8s中运行使用下面的
+        root="/app/userData/modelData/data/cifar10",
         train=False,
         download=True,
         transform=transform
---
+++ main4.py
@@ -0,0 +1,51 @@
+import torch
+from torch import optim
+
+from estimator import EstimatorFactory
+from method import evaluate, load_config
+from model import load_model
+from data.load_dataset import load_cifar
+from attack import AttackFactory
+
+
+# v4:模型代码，模型权重，模型配置文件位置适配k8s的pod目录
+def main():
+
+    # 1.加载配置文件
+    user_config = load_config("/app/userData/modelData/userConfig.yaml")
+    model_instantiation_config = user_config["model"]["instantiation"]
+    model_estimator_config = user_config["model"]["estimator"]
+    attack_config = user_config["attack"]
+
+    # 2.初始化模型
+    model = load_model(model_instantiation_config["model_path"], model_instantiation_config["model_name"]
+                       , model_instantiation_config["weight_path"], model_instantiation_config["parameters"])
+
+    # 3.获取优化器和损失函数
+    optimizer = optim.Adam(model.parameters(), lr=0.01)
+    loss = torch.nn.CrossEntropyLoss()
+
+    # 4.生成估计器
+    estimator = EstimatorFactory.create(
+        model=model,
+        loss=loss,
+        optimizer=optimizer,
+        config=model_estimator_config
+    )
+
+
+    # 5.生成攻击对象
+    attack = AttackFactory.create(
+        estimator=estimator.get_core(),
+        config=attack_config
+    )
+
+    # 6.加载数据
+    test_loader = load_cifar()
+
+    # 7.进行评估
+    evaluate(test_loader, estimator, attack)
+
+
+if __name__ == "__main__":
+    main()
