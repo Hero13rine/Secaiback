67ed729ae759d1e04d0e80baeb0a2f8442edb15f
HeFasheng <2973051203@qq.com>
2025-06-10 20:58:11 +0800

    2025-6-10:提高cifar10识别准确率

--- data/load_dataset.py
+++ data/load_dataset.py
@@ -1,6 +1,7 @@
 from torchvision import datasets, transforms
 from torch.utils.data import DataLoader
 
+
 def load_mnist(batch_size=64):
     transform = transforms.Compose([
         transforms.ToTensor(),
@@ -15,20 +16,18 @@ def load_mnist(batch_size=64):
     return DataLoader(test_set, batch_size=batch_size, shuffle=False)
 
 
-
 def load_cifar(batch_size=64):
     # CIFAR-10专用归一化参数
     transform = transforms.Compose([
         transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465),
-                             (0.247, 0.243, 0.261))
     ])
 
     test_set = datasets.CIFAR10(
         # 本地运行使用下面的
-        # root="./data/cifar10",
+        root="./data/cifar10",
+        # root="/home/he/project_code/AI_sec/art/data/cifar10",
         # k8s中运行使用下面的
-        root="/app/userData/modelData/data/cifar10",
+        # root="/app/userData/modelData/data/cifar10",
         train=False,
         download=True,
         transform=transform
